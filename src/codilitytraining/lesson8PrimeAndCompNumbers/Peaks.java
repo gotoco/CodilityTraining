d69ef538b612c7bf5719e5c2b74e1fb78432aa08
skirki <Maciej.grochowski89@gmail.com>
2014-11-20 00:18:51 +0100

    codility performance bug :)

--- src/codilitytraining/lesson8PrimeAndCompNumbers/Peaks.java
+++ src/codilitytraining/lesson8PrimeAndCompNumbers/Peaks.java
@@ -60,6 +60,7 @@ package codilitytraining.lesson8PrimeAndCompNumbers;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+import java.util.Random;
 
 public class Peaks {
 
@@ -67,30 +68,69 @@ public class Peaks {
 
         Peaks p = new Peaks();
 
-        int [] firstTestCase = {1, 2, 3, 4, 3, 4, 1, 2, 3, 4, 6, 2};
+//        int [] firstTestCase = {1, 2, 3, 4, 3, 4, 1, 2, 3, 4, 6, 2};
+//
+//        System.out.println("First test case should return 3 : " + p.solution(firstTestCase));
+//
+//        int [] secondEmptyCase = {1, 1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1};
+//
+//        System.out.println("Second test case should return 0 : " + p.solution(secondEmptyCase));
+//
+//        int [] smallCase = {1,2,3};
+//
+//        System.out.println("Small case should return 0 : " + p.solution(smallCase));
+//
+//        int [] normalCase = {1,2,3, 1,2,3, 1,2,3};
+//
+//        System.out.println("normalCase should return 1 : " + p.solution(normalCase));
+//
+//        int [] nastyCase = {1,2,3, 1,2,3, 1,2,3, 1};
+//
+//        System.out.println("nastyCase should return 2 : " + p.solution(nastyCase));
+//
+//
+        int [] nastyCase2 = {67,79,1,44,93,77,7,18,68,71,75,13,73,75,35,  };
 
-        System.out.println("First test case should return 3 : " + p.solution(firstTestCase));
+        for(int j=0; j<nastyCase2.length; j++){
+            System.out.print(nastyCase2[j]);
+            System.out.print(",");
+        }
+        System.out.println("");
 
-        int [] secondEmptyCase = {1, 1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1};
-
-        System.out.println("Second test case should return 0 : " + p.solution(secondEmptyCase));
-
-        int [] smallCase = {1,2,3};
-
-        System.out.println("Small case should return 0 : " + p.solution(smallCase));
-
-        int [] normalCase = {1,2,3, 1,2,3, 1,2,3};
-
-        System.out.println("normalCase should return 1 : " + p.solution(normalCase));
-
-        int [] nastyCase = {1,2,3, 1,2,3, 1,2,3, 1};
-
-        System.out.println("nastyCase should return 2 : " + p.solution(nastyCase));
+        //System.out.println("nastyCase2 should return 4 : " + p.solution(nastyCase2));
+        System.out.println("nastyCase2 should return 4 : " + p.brutForceSolution(nastyCase2));
 
 
-        int [] nastyCase2 = {1, 4, 3, 4, 2, 4, 1, 4, 3, 3};
+        int size = 10000 ;//Integer.MAX_VALUE;
 
-        System.out.println("nastyCase2 should return 4 : " + p.solution(nastyCase2));
+        for(int i=0 ; i<size; i++){
+
+            Random random = new Random() ;
+            int randomNumber = random.nextInt(150);
+
+            int sizeArray = randomNumber;
+            int [] array = new int[sizeArray];
+
+            for(int j=0; j<array.length; j++){
+                array[j] = random.nextInt(100) ;
+            }
+
+            int sol1 =  p.solution(array) ;
+            int sol2 = p.brutForceSolution(array) ;
+
+            if(sol1 != sol2){
+                System.out.println("  ");
+                System.out.println("sol1 =  " + sol1);
+                System.out.println("sol2 =  " + sol2);
+                for(int k=0;k<sizeArray;k++){
+                    System.out.print(array[k]);
+                    System.out.print(",");
+                }
+                System.out.println("  ");
+            }
+
+        }
+
     }
 
        /*Codility 100%*/
@@ -111,6 +151,12 @@ public class Peaks {
             }
         }
         peaksNumbers[N-1] =  peaksNumbers[N-2];
+
+        System.out.println("");
+            for(int j=0; j<peaksNumbers.length; j++){
+                System.out.print(peaksNumbers[j]);
+                System.out.print(",");
+            }
 
         List<Integer> divisors = new ArrayList<Integer>();
 
@@ -139,7 +185,7 @@ public class Peaks {
         return 0;
     }
 
-
+    /* This is funny but this also get on Codility 100%*/
     public int brutForceSolution(int [] A){
         int result = 0;
         int N = A.length;
@@ -171,11 +217,11 @@ public class Peaks {
             int ileCzesci = div;
             int elems = N/div;
             boolean nextDiv=false;
-            for(int s=1; s<N; s+=elems){
+            for(int s=0; s<N; s+=elems){
 
                 for(int k=s; k<s+elems; k++){
 
-                    if(k+1 < N)
+                    if(k+1 < N && k-1>=0)
                         if(A[k] > A[k-1] && A[k] > A[k+1]){
                             break;
                         }
